0b85c02faaa21603cdcab086522a5023aad56d7d
Otis Mohr <mail@otismohr.de>
2022-04-20 17:26:57 +0200

    Firebase Setup for Android and Web

--- android/app/src/main/kotlin/com/example/kilogram/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kilogram/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kilogram
+package de.otismohr.kilogram
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kilogram/layout/mobile_screen_layout.dart';
 import 'package:kilogram/layout/responsive_layout_screen.dart';
 import 'package:kilogram/layout/web_screen_layout.dart';
 import 'package:kilogram/utils/app_colors.dart';
 
-void main() {
+import 'api/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
