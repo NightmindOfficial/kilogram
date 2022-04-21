054e3ed171e67acae7dad517d7c5f5037dcad393
Otis Mohr <mail@otismohr.de>
2022-04-21 02:27:46 +0200

    Snackbar Improvements

--- lib/utils/snackbar_creator.dart
+++ lib/utils/snackbar_creator.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 showSnackbar(String message, BuildContext context) {
-  ScaffoldMessengerState().removeCurrentSnackBar();
+  ScaffoldMessenger.of(context).hideCurrentSnackBar();
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       content: Text(message),
